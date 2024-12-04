c0368ea2a5c58970279f50cebdbc059a71893889
umar hayat <umarhayat.coder@gmail.com>
2024-12-04 20:56:11 +0500

    Python String Practice

---
+++ Python Class/2. Class-03/String/practice.py
@@ -0,0 +1,39 @@
+# userName="Umar Hayat"
+# print(f"datatype of userName: {type(userName)}")
+
+#lenght
+# print(f"the lenght of userName: {len(userName)}")
+
+# Conversion
+# integer=2344534
+# print(f"the lenght of integer: {len(str(integer))}")
+
+# Contatenation
+# firstName="Umar"
+# lastName="hayat"
+# print(firstName,lastName)
+# fullName=firstName+" "+lastName
+# print(fullName)
+
+# Repitation
+# firstName="Umar"
+# print(firstName * 4)
+
+
+
+# Multiple value
+# userName="Umar Hayat " *2
+# print(userName*2)
+
+# Access character
+# character=userName[1]
+# print(character)
+# print("nega mean start from right: ",userName[-4])
+# print(userName[2:6])
+
+# Method of string
+userName="umar hayat zain ashfaq"
+# print(f"upper case: {userName.upper()}")
+# print(f"lower case: {userName.lower()}")
+# print(f"capitalizs case: {userName.capitalize()}")
+# print(f"title case: {userName.title()}")
