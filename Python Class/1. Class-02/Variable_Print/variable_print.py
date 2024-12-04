b197ae50e40f1ae416c120261ce20b2755626e40
umar hayat <umarhayat.coder@gmail.com>
2024-12-04 20:40:22 +0500

    Python Variable and Print Statement

---
+++ Python Class/1. Class-02/Variable_Print/variable_print.py
@@ -0,0 +1,27 @@
+# print
+
+print("hello world")
+print("hi , umar")
+
+#  variable
+num1=10*2+8-25/5
+print(num1)
+User_name="umar"        # Snack method
+print(User_name)
+userName="Zain"         # Camel method
+print(userName)
+UserName="Mahad"        # Pascal method
+print(UserName)
+
+
+textData="text Data"
+textData="True"
+textData="12344"
+textData="3,45"             # if we use same var then it over writes
+print(textData)
+
+# get input from user
+
+a=int(input("Enter a number"))
+print(a)
+
