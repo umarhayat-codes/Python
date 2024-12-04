693c0cbca35af2299b3ffc087ecf74a36d1a3fb4
umar hayat <umarhayat.coder@gmail.com>
2024-12-04 20:37:57 +0500

    Python Data_Type

---
+++ Python Class/1. Class-02/Data_Type_Operator/datatype_operator.py
@@ -0,0 +1,104 @@
+# Data type 
+
+# 1. String
+# 2. Integer
+# 3. Boolean
+# 4. Float
+
+                        # STRING
+
+# We can present by in 4 way
+
+# 1. single qout
+textData='text'
+print(textData)
+
+# Why we use it
+
+# textData1='Quaid Azam say, 'Anything is possible '
+textData1='Quaid Azam say, "Anything is possible" '
+print(textData1)
+
+# 2. Double qout
+
+doubleQout="text data"
+print(doubleQout)
+
+# Why we use it
+
+textData2="I'm very good"
+# textData2="I"m very good"
+print(textData2)
+
+# 3.Triple quot
+
+textData4='''Hello
+                    Hi
+                            A.o.a'''
+print(textData4)
+
+# 4. f string
+
+a=34
+print(f"the value of a :{a}")
+
+# We present  in string many data type
+
+textData="hi"
+textData="45"
+textData="True"
+print(textData)         # if we use same variable then it overwrites "mean that print latest value"
+
+# Apply operator
+
+a="hi"
+b=3
+print(a+b)
+
+a="hi"
+b="hello"
+print(a*b)
+
+
+                            # NUMERIC
+
+
+c=3
+print(c)
+d=3.4
+print(d)
+
+                            # OPERATOR
+e1=10
+e2=5
+print(e1+e2)
+print(e1/e2)
+print(e1*e2)
+print(e1%e2)
+
+result=e1/e2+15*e2-5.0
+print(result)
+
+result=result/5+8*5.3
+print(result)
+
+
+
+
+                            # BOOLEAN
+
+e=True
+print(e)
+
+
+
+
+
+
+
+
+
+
+
+
+
