70e8eebc5d1d9b941cf93ec98cdd80169bf363e4
umar hayat <umarhayat.coder@gmail.com>
2024-12-04 20:54:21 +0500

    Python All Operator

---
+++ Python Class/2. Class-03/Operator/operator.py
@@ -0,0 +1,34 @@
+# OPERATORS
+
+# TYPE OF OPERATOR
+
+# 1.Arithmetic operator
+# 2.Logical OPERATOR
+# 3.Comparsion operator
+# 4.Membership operator
+# 5.Identity operator
+# 6.Assigment Operator
+                        
+                        # 1. Arithmetic Operator
+
+# Precedence of arithmetic operator
+# 1.()            2. **           3. %, *, /, //              4. +, - 
+
+                        # 2.Comparsion Operator
+                        
+# 1. ==       2.!=        3.<=        4.>=        
+                        
+                        # 3. Logical Operator
+
+# 1.or (||)         2. and (&&)     3.not (!)
+
+                        # 4. Membership Operator
+
+# 1. in
+
+                        # 5. Identity Operator
+# 1. is
+
+                        # 5. Assigment Operator
+# 1. +=       2. -=       3. *=       4. /=
+
