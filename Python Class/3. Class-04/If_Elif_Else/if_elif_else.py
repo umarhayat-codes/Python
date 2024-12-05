21378a060c1dc2d253ed988762daf535046cf161
umar hayat <umarhayat.coder@gmail.com>
2024-12-05 20:19:38 +0500

    Python Condition Statement

---
+++ Python Class/3. Class-04/If_Elif_Else/if_elif_else.py
@@ -0,0 +1,200 @@
+# print("Hello World:")         i want to it is not execute
+# isGreater=5>10
+# if isGreater:
+#     print("Hello World:")
+
+# n=52
+# if  n%2==0:
+#     print("Even")
+#     if n>50:
+#         print("Greater than 50")
+
+# n=0
+# if  n==0:
+#     print("Zero")
+# elif n%2==0:
+#     print("Even")
+# else:
+#     print("odd")
+
+# Exercise of if else in it series book
+
+# marks=int(input("Enter the mark"))
+# if marks>=40:
+#     print("Congratulation")
+
+# a=int(input("Enter a number"))
+# b=int(input("Enter a number"))
+# if a==b:
+#     print("Both number are equal")
+
+
+# a=int(input("Enter a number"))
+# b=int(input("Enter a number"))
+# if a*a==b:
+#     print("First number is square of Second")
+
+# sub1=int(input("Enter the mark"))
+# sub2=int(input("Enter the mark"))
+# sub3=int(input("Enter the mark"))
+# avg=(sub1+sub2+sub3)/3
+# if avg>=80:
+#     print("You are above standard")
+#     print("congratulation")
+
+# a=int(input("Enter a number"))
+# b=int(input("Enter a number"))
+# c=int(input("Enter a number"))
+# max=a
+# if b>max:
+#     max=b
+# if c>max:
+#     max=c
+# print("The maximum number is:", max)
+
+# a=int(input("Enter a number"))
+# if a>0:
+#     print("Postive")
+# if a<0:
+#     print("Negative")
+# if a==0:
+#     print("Zero")
+
+# a=int(input("Enter a number"))
+# b=int(input("Enter a number"))
+# c=int(input("Enter a number"))
+# min=max=a
+# if b>max:
+#     max=b
+# if c>max:
+#     max=c
+# if b<min:
+#     min=b
+# if c<min:
+#     min=c
+
+# print("The maximum number is:", max)
+# print("The minimum number is:", min)
+
+# 'if-else'
+
+# a=int(input("Enter a number"))
+# if a % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# a=int(input("Enter a number"))
+# if (a % 4 ==0):
+#     print("Leap year")
+# else:
+#     print("Not Leap year")
+
+# sal=int(input("Enter your salary -->"))
+# grade=int(input("Enter your grade -->"))
+# if grade>15:
+#     bonus=sal * 50.0/100.0
+# else:
+#     bonus=sal * 25.0/100.0
+# netSalary=sal+bonus
+# print("The total Salary is :", netSalary)
+
+
+# a=int(input("Enter a number"))
+# b=int(input("Enter a number"))
+# if a%b==0:
+#     print("First integer is multiple of second:")
+# else:
+#     print("First integer is not multiple of second:")
+
+# 'if-else-if'
+
+# score=int(input("Enter your score -->"))
+# if score>=90:
+#     print("Grade :A")
+# elif score>=80:
+#     print("Grade:B")
+# elif score>=70:
+#     print("Grade:C")
+# elif score>=60:
+#     print("Grade:D")
+# else:
+#     print("Grade:F;")
+
+# unit=int(input("Enter the unit -->"))
+# if unit>500:
+#     bill=unit * 7
+# elif unit>300:
+#     bill=unit*5
+# else:
+#     bill=unit*2
+# bill=bill+150
+# if bill>2000:
+#     bill=bill+(bill*5/100)
+# print("The total bill generate is:", bill)   
+
+# sal=int(input("Enter the salary-->"))
+# if sal>=20000:
+#     net=sal-(sal*7/100)
+# elif sal>=10000:
+#     net=sal-1000
+# else:
+#     net=sal
+# print("The net salary is :",net)
+
+# a=int(input("Enter a number"))
+# b=int(input("Enter a number"))
+# c=int(input("Enter a number"))
+# if a>b:
+#     if a>c:
+#         print("A is greater:")
+#     else:
+#         print("C is greater:")
+# else:
+#     if b>c:
+#         print("B is greater:")
+#     else:
+#         print("C is greater: ")
+
+# a=int(input("Enter a number"))
+# b=int(input("Enter a number"))
+# c=int(input("Enter a number"))
+# if a==b:
+#     if a==c:
+#         print("All number is equal:")
+#     else:
+#         print("All number is different")
+# else:
+#     print("All number is different")
+
+# Logical Operator
+
+
+# a=int(input("Enter a number"))
+# b=int(input("Enter a number"))
+# c=int(input("Enter a number"))
+
+# if a>b and a>c:
+#     print("A is greater")
+# elif b>a and b>c:
+#     print("B is greater")
+# else:
+#     print("C is greater")
+
+# ch=input("Enter any vowel -->")
+# if (ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='I' or ch=='i' or ch=='o' or ch=='O' ):
+#     print("Your enter is vowel character -->")
+# else:
+#     print("no vowel character -->")
+ 
+# ch=input("Enter any character or number or symbol -->")
+# if ch>='A' and ch<='Z':
+#     print("Your enter is capital character -->")
+# elif ch>='a' and ch<='z':
+#     print("Your enter is small character -->")
+# elif ch>='0' and ch<='9':
+#     print("Your enter is number -->")
+# else:
+#     print("your enter an symbol -->")
+
+
