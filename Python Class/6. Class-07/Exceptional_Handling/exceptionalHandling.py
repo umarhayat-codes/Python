02713926e3b9b7263df073e3343ed440d3d6d8fe
Umar Hayat <umarhayat.coder@gmail.com>
2024-12-12 21:22:13 +0500

    Implemented exception handling for error management

---
+++ Python Class/6. Class-07/Exceptional_Handling/exceptionalHandling.py
@@ -0,0 +1,47 @@
+num1 = int(input("Enter number 1: "))
+
+
+# Value Error
+# try:
+#     userInput=int(input("Enter the string value: "))
+# except Exception as e:
+#     print(f"Error: {e}")
+# else:
+#     print(f"User Input is correct : {userInput}")
+# finally:
+#     print("Completly Execution: ")
+
+# dictionary key:value
+# try:
+#     dic={"keys":"values"}
+#     value=dic["name"]
+# except Exception as e:
+#     print(f"Error: {e}")
+# else:
+#     print(f"The value is: {value}")
+# finally:
+#     print(f"Completely Execution: ")
+
+# Tpye error
+# try:
+#     n1=int(input("Enter the umber"))
+#     n2=int(input("Enter the umber"))
+#     result= n1+n2
+# except Exception as e:
+#     print(f"Error: {e}")
+# else:
+#     print(f"the result is {result}")
+# finally:
+#     print("Completly Execution: ")
+
+# try:
+#     my_list = [1, 2, 3]
+#     length = my_list.len()
+# except Exception as e:
+#     print(f"Error: {e}")
+
+# try:
+#     with open("text.txt","r") as file:
+#         text=file.read()
+# except Exception as e:
+#     print(f"Error: {e}")
