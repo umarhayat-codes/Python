d1c24558350730b4839817679755e80a8913825b
Umar Hayat <umarhayat.coder@gmail.com>
2024-12-15 11:36:41 +0500

    Implemented *args and **kwargs to handle variable-length arguments in functions, with examples for dynamic positional and keyword arguments.

---
+++ Python Class/9. Class-10/OOP/arg_kward.py
@@ -0,0 +1,21 @@
+# *arg 
+# treat as tuple and pass position argument
+
+# def add(*args):
+    # result = 0
+    # for arg in args:
+    #     result += arg
+    # return result
+    # return type(args)
+# print((add(1,2,3,4,5,6,7)))
+
+
+# def greet(**kwards):
+#     if 'name' in kwards:
+#         print(f"Hello, {kwards['name']}")
+#     else:
+#         print("Hello, anonymous")
+    # print(type(kwards))
+# greet()
+# greet(name = "umar")
+# greet(name = "zain")
